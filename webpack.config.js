9d619ea5c247988d8a23d04e4e38435a0d9048fd
Arunkumar Srisailapathi <aarunsri@amazon.com>
2020-08-15 19:50:55 +0530

    brings in copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 // Webpack uses this to work with directories
 const path = require('path');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 // This is the main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
   // Path to your entry point. From this file Webpack will begin his work
   entry: './src/index.js',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, 'package.json') }],
+    }),
+  ],
 
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
