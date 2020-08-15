9a5cb092bc507ae8dc7b093be759f222cbdce82c
Arunkumar Srisailapathi <aarunsri@amazon.com>
2020-08-15 19:38:32 +0530

    publishes dist

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// Webpack uses this to work with directories
+const path = require('path');
+
+// This is the main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './src/index.js',
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript
+  // minifying and other thing so let's set mode to development
+  mode: 'development',
+};
